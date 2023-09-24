e335cc17d6922d70de8f49aa641902743946b800
MaliaPagley <maliapagley@gmail.com>
2023-09-24 15:04:29 -0400

    Update Populargames.jsx
    Added:
    - Views
    - Styles
    - UseState: isLoading
    - Ternany Operator: Error, ActivityIndicator, FlatList
    - PopularGameCard component

--- components/home/popular/Populargames.jsx
+++ components/home/popular/Populargames.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState }from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator, Touchable } from 'react-native'
+import { useRouter } from 'expo-router'
 
-import styles from './populargames.style'
+import { COLORS, SIZES } from '../../../constants';
+import styles from './populargames.style';
+import PopularGameCard from '../../common/cards/popular/PopularGameCard';
+
 
 const Populargames = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const error = false;
   return (
-    <View>
-      <Text>Popular Games</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Games</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+      {isLoading ? (
+        <ActivityIndicator size="large" colors={COLORS.primary} />
+      ) : error ? (
+        <Text>Something went wrong</Text>
+      ) : (
+        <FlatList
+          data={[1,2,3,4,5,6,7,8,9,10]}
+          renderItem={({item}) => (
+            <PopularGameCard
+              item={item}
+            />
+          )}
+          keyExtractor={item => item?.game_id}
+          // ^ Unknown id selection for exact "game_id" until we get the data from IGDB API 
+          contentContainerStyle={{ columnGap: SIZES.medium}}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      )}
+    
+      </View>
     </View>
   )
 }
