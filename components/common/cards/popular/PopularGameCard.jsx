46120b72b61113435b01e477d45db714ce0acd4b
MaliaPagley <maliapagley@gmail.com>
2023-10-06 14:17:27 -0400

    Added No image handling
    Details:
    - (ADDED) utils folder / checkImageUrl
    - (ADDED) NoImage.png
    - (UPDATE) PopularGameCard for conditionally rendering images

--- components/common/cards/popular/PopularGameCard.jsx
+++ components/common/cards/popular/PopularGameCard.jsx
@@ -1,7 +1,9 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./populargamecard.style";
-// import { checkImageURL } from "../../../../utils";
+import { checkImageURL } from "../../../../utils";
+
+const NoImage = require("../../../../assets/noimage.png")
 
 const PopularGameCard = ({ item, selectedJob, handleCardPress }) => {
   // console.log(item.results)
@@ -12,16 +14,25 @@ const PopularGameCard = ({ item, selectedJob, handleCardPress }) => {
       onPress={() => handleCardPress(item)}
     >
       <TouchableOpacity >
+        {checkImageURL(item.background_image) ?  
         <Image
           source={{
-            uri: item?.background_image
-             
+            uri: item.background_image,
           }}
           resizeMode='center'
           style={styles.logoImage}
-        />
+      /> 
+      :
+      <Image
+          source={NoImage}
+          resizeMode='contain'
+          style={styles.logoImage}
+      /> 
+      }
+    
+        
       </TouchableOpacity>
-      <Text style={styles.gameName} numberOfLines={1}>
+      <Text style={styles.gameName} numberOfLines={2}>
         {item.name}
       </Text>
 
