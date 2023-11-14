9d73d2436e525bb1346069958d08bd4017646a70
MaliaPagley <maliapagley@gmail.com>
2023-11-14 15:59:06 -0500

    Update profile style
    Details:
    (UPDATE) stylesheet and layout

--- app/home/profile.js
+++ app/home/profile.js
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '../../context/auth'
+import { COLORS, FONT } from '../../constants'
+import { Ionicons } from '@expo/vector-icons';
 
-export default function profile() {
+const styles = StyleSheet.create({
+  container: {
+      // flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+  },
+  profileContainer: {
+    color: COLORS.white,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  email: {
+    color: COLORS.white,
+    fontFamily: FONT.bold
+  },
+  signOut: {
+    color: COLORS.actionBlue
+  }
+})
+
+const Profile = () => {
+  const { signOut, user } = useAuth();
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView style={styles.container}> 
+      <View style={styles.profileContainer}>
+      <Ionicons name={'person-circle-outline'} size={100} color="white" />
+        <Text style={styles.email}>{user.email}</Text>
+       
+        <Button 
+                title="Sign Out"
+                onPress={() => signOut()}
+            />
+     </View>
+    </SafeAreaView>
   )
 }
+
+export default Profile
