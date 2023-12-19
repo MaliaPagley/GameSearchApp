6360a7e71db2deea527a405de0282e7523a6106d
MaliaPagley <maliapagley@gmail.com>
2023-12-19 15:31:25 -0500

    Update search
    Details:
    -(UPDATE) [id] for GameSearch component structure change and styles.
    -(ADDED) useSearch hook for fetching data for searching.

--- app/search/[id].js
+++ app/search/[id].js
@@ -1,133 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
-import { Stack, useRouter, useLocalSearchParams } from 'expo-router'
-import { Text, SafeAreaView } from 'react-native'
-import axios from 'axios'
-import { ScreenHeaderBtn, NewGameCard } from '../../components'
-import { COLORS, icons, SIZES } from '../../constants'
-import styles from '../../styles/search.style'
-import { CURRENT_HOST } from '../../utils/host'
-
+import React from 'react';
+import { ActivityIndicator, FlatList, SafeAreaView, Text, View } from 'react-native';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { ScreenHeaderBtn, NewGameCard } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import styles from '../../styles/search.style';
+import useSearch from '../../hook/useSearch';
 
 const GameSearch = () => {
-    const params = useLocalSearchParams();
-    const router = useRouter();
+  const params = useLocalSearchParams();
+  const { searchLoader, searchError, searchResult } = useSearch(`${params.id}`)
+  const router = useRouter();
 
-    const [searchResult, setSearchResult] = useState([]);
-    const [searchLoader, setSearchLoader] = useState(false);
-    const [searchError, setSearchError] = useState(null);
-    const [page, setPage] = useState(1);
+  const handleCardPress = (item) => {
+    router.push(`/game-details/${item.id}`);
+  };
 
-
-const handleSearch = async () => {
-  setSearchLoader(true);
-  setSearchResult([]);
-
-  try {
-    const options = {
-      method: 'GET',
-      url: `http://${CURRENT_HOST}:8000/search/${params.id}`,
-    };
-
-    const response = await axios.request(options);
-        const res = response.data.results
-            setSearchResult(res);
-            
-  } catch (error) {
-    setSearchError(error);
-    console.log(error);
-  } finally {
-    setSearchLoader(false);
-  }
-};
-
-
-    const handlePagination = (direction) => {
-        if (direction === 'left' && page > 1) {
-            setPage(page - 1)
-            handleSearch()
-        } else if (direction === 'right') {
-            setPage(page + 1)
-            handleSearch()
-        }
-    }
-
-    useEffect(() => {
-        handleSearch()
-    }, [])
-
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.blackMirage }}>
-            <Stack.Screen
-                options={{
-                    headerStyle: { backgroundColor: COLORS.blackMirage },
-                    headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn
-                            iconUrl={icons.left}
-                            dimension='60%'
-                            handlePress={() => router.back()}
-                        />
-                    ),
-                    headerTitle: "",
-                }}
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.blackOnyx }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.blackOnyx },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension='60%'
+              handlePress={() => router.back()}
             />
-               
+          ),
+          headerTitle: "",
+        }}
+      />
+       <View>
+       <View style={styles.container}>
+              <Text style={styles.searchTitle}>{params.id}</Text>
+              <Text style={styles.noOfSearchedGame}>Search Results:</Text>
+            </View>
+           
+        {searchLoader ? (
+          <ActivityIndicator style={styles.loading} testID='loading-indicator' size='large'  />
+        ) : searchError ? (
+          <Text style={styles.error}>Data Unavailable</Text>
+        ) : (
+          <View > 
             <FlatList
+              testID='list-id'
                 data={searchResult}
                 renderItem={({ item }) => (
-                    <NewGameCard
-                        game={item}
-                       handleCardPress={() => router.push(`/game-details/${item.id}`)}
-                    />
+                  <NewGameCard 
+                    game={item}
+                    handleCardPress={() => handleCardPress(item)} />
                 )}
-                keyExtractor={(item) => item.id}
-                contentContainerStyle={{ padding: SIZES.medium, rowGap: SIZES.medium }}
-                ListHeaderComponent={() => (
-                    <>
-                        <View style={styles.container}>
-                            <Text style={styles.searchTitle}>{params.id}</Text>
-                            <Text style={styles.noOfSearchedGame}>Search Results</Text>
-                        </View>
-                        <View style={styles.loaderContainer}>
-                            {searchLoader ? (
-                                <ActivityIndicator size='large' color={COLORS.white} />
-                            ) : searchError && (
-                                <Text>Oops something went wrong</Text>
-                            )}
-                        </View>
-                    </>
-                )}
-                ListFooterComponent={() => (
-                    <View style={styles.footerContainer}>
-                        <TouchableOpacity
-                            style={styles.paginationButton}
-                            onPress={() => handlePagination('left')}
-                        >
-                            <Image
-                                source={icons.chevronLeft}
-                                style={styles.paginationImage}
-                                resizeMode="contain"
-                            />
-                        </TouchableOpacity>
-                        <View style={styles.paginationTextBox}>
-                            <Text style={styles.paginationText}>{page}</Text>
-                        </View>
-                        <TouchableOpacity
-                            style={styles.paginationButton}
-                            onPress={() => handlePagination('right')}
-                        >
-                            <Image
-                                source={icons.chevronRight}
-                                style={styles.paginationImage}
-                                resizeMode="contain"
-                            />
-                        </TouchableOpacity>
-                    </View>
-                )}
-            />
-        </SafeAreaView>
-    )
-}
+                contentContainerStyle={{ padding: SIZES.small }}
+                keyExtractor={(item, index) => `search-game-${item.id}-${index}`}
+              />
+             
+          </View>
+          )}
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default GameSearch
+export default GameSearch;
---
+++ hook/useSearch.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { CURRENT_HOST } from '../utils/host';
+
+const useSearch = (params) => {
+  const [searchLoader, setSearchLoader] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchError, setSearchError] = useState(null);
+
+  const handleSearch = async () => {
+    setSearchLoader(true);
+    setSearchResult([]);
+
+    try {
+      const options = {
+        method: 'GET',
+        url: `http://${CURRENT_HOST}:8000/search/${params}`,
+      };
+
+      const response = await axios.request(options);
+      const res = response.data.results;
+      setSearchResult(res);
+    } catch (error) {
+      setSearchError(error);
+      console.log(error);
+    } finally {
+      setSearchLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
+  return { searchLoader, searchResult, searchError, handleSearch };
+};
+
+export default useSearch;
