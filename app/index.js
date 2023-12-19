6be98ba9c0f9f7b0f46c01a1061b52455c6f03ff
MaliaPagley <maliapagley@gmail.com>
2023-12-19 14:00:30 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,40 +1,22 @@
-import { View, ScrollView, SafeAreaView, StatusBar } from 'react-native'
-import { useState } from 'react';
-import { useRouter } from 'expo-router'
-
-import { COLORS, SIZES } from '../constants'
-import { Newgames, Populargames, Welcome } from '../components'
+import React from 'react';
+import { View, ScrollView, SafeAreaView, StatusBar } from 'react-native';
+import { Newgames, Populargames, Welcome } from '../components';
 
 const Home = () => {
-    const router = useRouter()
-    const [searchTerm, setSearchTerm] = useState(" ")
+  return (
+    <SafeAreaView>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{
+          flex: 1,
+          padding: 15
+        }}>
+          <Welcome />
+          <Populargames />
+          <Newgames />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgoundColor: COLORS.blackMirage}}> 
-            <ScrollView style={{backgroundColor: COLORS.blackOnyx}}
-            showsVerticalScrollIndicator={false}>
-                <View style={{
-                    flex: 1,
-                    padding: SIZES.medium
-                    }}
-                >
-                    <Welcome 
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        handleClick={() => {
-                            if(searchTerm) {
-                                router.push(`/search/${searchTerm}`)
-                            }
-                        }}
-                    
-                    />
-                    <Populargames />
-                    <Newgames />
-                    <StatusBar barStyle={'light-content'}/>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
 export default Home;
-
