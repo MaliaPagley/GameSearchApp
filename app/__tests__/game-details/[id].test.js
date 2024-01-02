5d9f97935154d041123777fee0fc062c683ae45d
MaliaPagley <maliapagley@gmail.com>
2024-01-01 21:42:09 -0500

    Update game-details/[id] test
    Details:
    -(UPDATE) GameDetails component.
    -(ADDED) GameDetails testing.

---
+++ app/__tests__/game-details/[id].test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import GameDetails from '../../game-details/[id]';
+import { useLocalSearchParams } from 'expo-router';
+import useFetch from '../../../hook/useFetch';
+
+jest.mock('../../../hook/useFetch');
+jest.mock('expo-router');
+
+describe('GameDetails Component: ', () => {
+  const mockUseFetchResponse = {
+    data: {
+      name: 'Test Game',
+      background_image: 'https://test.com/test-image.jpg',
+      developers: [{ name: 'Test Developer' }],
+      released: '2024-01-01',
+      tags: ['Multiplayer', 'Singleplayer'],
+      platforms: [
+        { platform: { name: 'PlayStation 5' } },
+        { platform: { name: 'Xbox One' } },
+      ],
+      genres: [{ name: 'Action' }, { name: 'Adventure' }],
+      description_raw: 'This is a test game description.',
+    },
+    isLoading: false,
+    error: null,
+  };
+
+  it('renders component correctly', () => {
+    useLocalSearchParams.mockReturnValue({ id: '123' });
+    useFetch.mockReturnValue(mockUseFetchResponse);
+
+    const { getByTestId, getByText } = render(<GameDetails />);
+
+    const name = getByText('Test Game');
+    const developers = getByText('Test Developer');
+    const date = getByText('Release Date: 2024-01-01');
+    const platformOne = getByTestId('playstation-icon');
+    const platformTwo = getByTestId('xbox-icon');
+    const genreOne = getByText('Action');
+    const genreTwo = getByText('Adventure');
+    const description = getByText('This is a test game description.');
+
+    expect(name).toBeDefined();
+    expect(developers).toBeDefined();
+    expect(date).toBeDefined();
+    expect(platformOne).toBeTruthy();
+    expect(platformTwo).toBeTruthy();
+    expect(genreOne).toBeDefined();
+    expect(genreTwo).toBeDefined();
+    expect(description).toBeDefined();
+  });
+
+  it('renders loading indicator', () => {
+    useLocalSearchParams.mockReturnValue({ id: '123' });
+    useFetch.mockReturnValue({ isLoading: true, error: null, data: null });
+
+    const { getByTestId } = render(<GameDetails />);
+    const activityIndicator = getByTestId('loadingID');
+    expect(activityIndicator).toBeTruthy();
+  });
+
+  it('renders error message', () => {
+    useLocalSearchParams.mockReturnValue({ id: '123' });
+    useFetch.mockReturnValue({ isLoading: false, error: true, data: null });
+
+    const { getByText } = render(<GameDetails />);
+    expect(getByText('Something went wrong')).toBeDefined();
+  });
+});
--- app/game-details/[id].js
+++ app/game-details/[id].js
@@ -1,62 +1,37 @@
 import React from 'react';
-import { View, Text, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native';
-import { Stack, useRouter, useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
+import { View, Text, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
 import {
   GameHeader,
   GameAbout,
   GameFooter,
   GameTabOptions,
-  ScreenHeaderBtn,
   GamePlatforms,
   GameTags,
   GameGenres,
   GameFavoritesButton
 } from '../../components';
-import { COLORS, icons, SIZES } from '../../constants';
+import { COLORS, SIZES } from '../../constants';
+import styles from '../../styles/gamedetails.style'
 import useFetch from '../../hook/useFetch';
-
-const styles = {
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-   
-  },
-};
 
 const GameDetails = () => {
   const params = useLocalSearchParams();
-  const router = useRouter();
-  const { data, isLoading, error, refetch } = useFetch(`game-details/${params.id}`);
-
-  const [refreshing, setRefreshing] = useState(false);
-  const onRefresh = () => {};
+  const { data, isLoading, error } = useFetch(`game-details/${params.id}`);
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.blackOnyx }}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.blackOnyx },
-          headerShadowVisible: false,
-          headerBackVisible: false,
-          headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.left} dimension="60%" handlePress={() => router.back()} />
-          ),
-          headerTitle: '',
-        }}
-      />
-      <>
         <ScrollView
           showsVerticalScrollIndicator={false}
-          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
         >
           {isLoading ? (
             <View style={styles.loadingContainer}>
-              <ActivityIndicator size="large" color={COLORS.white} />
+              <ActivityIndicator testID='loadingID' size="large" color={COLORS.white} />
             </View>
           ) : error ? (
-            <Text>Something went wrong</Text>
+            <View style={styles.loadingContainer}>
+              <Text style={styles.error}>Something went wrong</Text>
+            </View>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
               <GameHeader 
@@ -75,7 +50,6 @@ const GameDetails = () => {
             </View>
           )}
         </ScrollView>
-      </>
     </SafeAreaView>
   );
 };
