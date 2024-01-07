0bba3b2c4fd32708eb43bb8f62b7a364656202ca
MaliaPagley <maliapagley@gmail.com>
2024-01-07 02:18:08 -0500

    Update sign-up.test.js

--- app/__tests__/(auth)/sign-up.test.js
+++ app/__tests__/(auth)/sign-up.test.js
@@ -3,49 +3,55 @@ import { render } from '@testing-library/react-native';
 import SignUp from '../../(auth)/sign-up';
 import useSignup from '../../../hook/useSignup';
 
-jest.mock('../../../hook/useSignup', () => ({
-  __esModule: true,
-  default: jest.fn().mockReturnValue({
+jest.mock('../../../hook/useSignup');
+
+describe('SignUp Component: ', () => {
+  useSignup.mockReturnValue({
     signUp: jest.fn(),
     loading: false,
-    error: null,
-  }),
-}));
-
-describe('SignUp Component:', () => {
-  it('renders correctly', () => {
-    const { getByText, getByPlaceholderText } = render(<SignUp />);
-
-    const headerTextOne = getByText('Create Account');
-    const headerTextTwo = getByText('Please fill in the fields.');
-    const FullName = getByPlaceholderText('Full Name');
-    const emailInput = getByPlaceholderText('Email');
-    const passwordInput = getByPlaceholderText('Password');
-
-    expect(headerTextOne).toBeDefined();
-    expect(headerTextTwo).toBeDefined();
-    expect(FullName).toBeDefined();
-    expect(emailInput).toBeDefined();
-    expect(passwordInput).toBeDefined();
   });
 
-  it('renders button correctly', () => {
-    const { getByTestId, getByText } = render(<SignUp />);
-
-    const signUpText = getByText('Sign Up');
-    const signUpButton = getByTestId('buttonID');
-
-    expect(signUpText).toBeDefined();
-    expect(signUpButton).toBeTruthy();
-  });
-
-  it('renders loading indicator', () => {
-    useSignup.mockReturnValue({
-      loading: true,
+  describe('when sign-up screen displays', () => {
+    it('renders headers correctly', () => {
+      const { getByText } = render(<SignUp />);
+  
+      const headerTextOne = getByText('Create Account');
+      const headerTextTwo = getByText('Please fill in the fields.');
+  
+      expect(headerTextOne).toBeDefined();
+      expect(headerTextTwo).toBeDefined();
     });
-    const { getByTestId } = render(<SignUp />);
-    const activityIndicator = getByTestId('loadingID');
 
-    expect(activityIndicator).toBeTruthy();
+    it('renders inputs correctly', () => {
+      const { getByPlaceholderText } = render(<SignUp />);
+  
+      const nameInput = getByPlaceholderText('Full Name');
+      const emailInput = getByPlaceholderText('Email');
+      const passwordInput = getByPlaceholderText('Password');
+  
+      expect(nameInput).toBeDefined();
+      expect(emailInput).toBeDefined();
+      expect(passwordInput).toBeDefined();
+    });
+
+    it('renders buttons correctly', () => {
+      const { getByTestId, getByText } = render(<SignUp />);
+  
+      const signUpText = getByText('Sign Up');
+      const signUpButton = getByTestId('buttonID');
+  
+      expect(signUpText).toBeDefined();
+      expect(signUpButton).toBeTruthy();
+    });
+  });
+
+  describe('when loading', () => {
+    it('renders loading indicator', () => {
+      useSignup.mockReturnValue({ loading: true });
+      const { getByTestId } = render(<SignUp />);
+      const activityIndicator = getByTestId('loadingID');
+
+      expect(activityIndicator).toBeTruthy();
+    });
   });
 });
