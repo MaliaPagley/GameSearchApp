5f85fc4abe6caa74a804602f9e190a7a619d1f3e
MaliaPagley <maliapagley@gmail.com>
2024-01-07 02:18:29 -0500

    Update profile.test.js

--- app/__tests__/profile/profile.test.js
+++ app/__tests__/profile/profile.test.js
@@ -5,59 +5,65 @@ import useUserData from '../../../hook/useUserData';
 
 jest.mock('../../../hook/useUserData');
 
-describe('Profile Component: ', () => {
-    const mockUserData = {
-        favorites: [
-          { id: 1, name: 'Favorite Game 1' },
-          { id: 2, name: 'Favorite Game 2' },
-        ],
-        fullName: 'Test User',
-        loading: false,
-        error: false,
-        handleSignOut: jest.fn(),
-        user: { email: 'test.user@mail.com' },
-      };
+describe('Profile Component', () => {
+  const mockUserData = {
+    favorites: [
+      { id: 1, name: 'Favorite Game 1' },
+      { id: 2, name: 'Favorite Game 2' },
+    ],
+    fullName: 'Test User',
+    loading: false,
+    error: false,
+    handleSignOut: jest.fn(),
+    user: { email: 'test.user@mail.com' },
+  };
 
-    it('renders user information', () => {
-        useUserData.mockReturnValueOnce(mockUserData);
-        const { getByText } = render(<Profile />);
+  it('renders user information correctly', () => {
+    useUserData.mockReturnValueOnce(mockUserData);
+    const { getByText } = render(<Profile />);
 
-        const fullName = getByText('Test User');
-        const email = getByText('test.user@mail.com');
-        const favoriteOne = getByText('Favorite Game 1');
-        const favoriteTwo = getByText('Favorite Game 2');
+    const fullName = getByText('Test User');
+    const email = getByText('test.user@mail.com');
+    const favoriteOne = getByText('Favorite Game 1');
+    const favoriteTwo = getByText('Favorite Game 2');
 
-        expect(fullName).toBeDefined();
-        expect(email).toBeDefined();
-        expect(favoriteOne).toBeDefined();
-        expect(favoriteTwo).toBeDefined();
+    expect(fullName).toBeDefined();
+    expect(email).toBeDefined();
+    expect(favoriteOne).toBeDefined();
+    expect(favoriteTwo).toBeDefined();
+  });
+
+  describe('when button is pressed', () => {
+    it('calls handleSignOut', async () => {
+      useUserData.mockReturnValue(mockUserData);
+
+      const { getByText } = render(<Profile />);
+      const signOutButton = getByText('Sign Out');
+      expect(signOutButton).toBeTruthy();
+
+      fireEvent.press(signOutButton);
+
+      await waitFor(() => {
+        expect(useUserData().handleSignOut).toHaveBeenCalled();
+      });
     });
+  });
 
-    it('renders signOut button and press ', async () => {
-        useUserData.mockReturnValue(mockUserData);
-
-        const { getByText } = render(<Profile />);
-        const signOutButton = getByText('Sign Out');
-        expect(signOutButton).toBeTruthy();
-
-        fireEvent.press(signOutButton);
-       
-        await waitFor(() => {
-            expect(useUserData().handleSignOut).toHaveBeenCalled();
-        });
-    });
-
+  describe('when loading', () => {
     it('renders loading indicator', () => {
-        useUserData.mockReturnValueOnce({ loading: true });
-        const { getByTestId } = render(<Profile />);
-        expect(getByTestId('loadingID')).toBeTruthy()
-    })
-
-    it('renders error message', () => {
-        useUserData.mockReturnValueOnce({ error: true});
-        const { getByText } = render(<Profile />);
-
-        expect(getByText('Something went wrong')).toBeDefined();
+      useUserData.mockReturnValueOnce({ loading: true });
+      const { getByTestId } = render(<Profile />);
+      
+      expect(getByTestId('loadingID')).toBeTruthy();
     });
+  });
 
+  describe('when on error', () => {
+    it('renders error message', () => {
+      useUserData.mockReturnValueOnce({ error: true });
+      const { getByText } = render(<Profile />);
+
+      expect(getByText('Something went wrong')).toBeDefined();
+    });
+  });
 });
