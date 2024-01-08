dfb088481d3905d2764fbaae2a280c4c7c26e298
MaliaPagley <maliapagley@gmail.com>
2024-01-07 23:40:27 -0500

    Update auth.js
    -(UPDATE) Linting: spacing, and quotes.

--- context/auth.js
+++ context/auth.js
@@ -1,44 +1,42 @@
-import { createContext, useContext, useEffect, useState } from 'react';
 import { useSegments, useRootNavigationState, useRouter } from "expo-router";
+import { createContext, useContext, useEffect, useState } from "react";
 
-
-const router = useRouter();
 const AuthContext = createContext(null);
 
 export function useAuthContext() {
-    return useContext(AuthContext) || {};
+  return useContext(AuthContext) || {};
 }
 
 function useProtectedRoute(user) {
-    const segments = useSegments();
-    const navigationState = useRootNavigationState();
+  const segments = useSegments();
+  const router = useRouter();
+  const navigationState = useRootNavigationState();
 
-    useEffect(() => {
-        if(!navigationState?.key) return;
-        const isAuthGroup = segments[0] === "(auth)";
+  useEffect(() => {
+    if (!navigationState?.key) return;
+    const isAuthGroup = segments[0] === "(auth)";
 
-        if (!user && !isAuthGroup) {
-            router.replace("/sign-in")
-        } else if (user && isAuthGroup) {
-            router.replace("/")
-        }
-
-    }, [user, segments, navigationState?.key])
-
+    if (!user && !isAuthGroup) {
+      router.replace("/sign-in");
+    } else if (user && isAuthGroup) {
+      router.replace("/");
+    }
+  }, [user, segments, navigationState?.key]);
 }
 
 export function Provider({ children }) {
-    const [user, setUser ] = useState(null);
-    useProtectedRoute(user);
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  useProtectedRoute(user);
 
-    const signOut = () => {
-        setUser(null);
-        router.replace('/sign-in');
-    };
+  const signOut = () => {
+    setUser(null);
+    router.replace("/sign-in");
+  };
 
-    return (
-        <AuthContext.Provider value={{ setUser, user, signOut}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ setUser, user, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
