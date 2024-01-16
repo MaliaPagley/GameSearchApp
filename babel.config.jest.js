1704c8699c686c307eb3c933dc2481fe0202835e
MaliaPagley <maliapagley@gmail.com>
2024-01-16 14:32:38 -0500

    Update babel.config.jest.js

--- babel.config.jest.js
+++ babel.config.jest.js
@@ -1,18 +1,20 @@
-module.exports = require('babel-jest').createTransformer({
+module.exports = require("babel-jest").createTransformer({
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
     "@babel/plugin-proposal-export-namespace-from",
     "react-native-reanimated/plugin",
     require.resolve("expo-router/babel"),
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-    }],
-    ['@babel/plugin-transform-flow-strip-types'], 
-    ['@babel/plugin-transform-private-methods', { "loose": true }],
-    ["@babel/plugin-transform-class-properties", { "loose": true }],
-    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
-
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+    ["@babel/plugin-transform-flow-strip-types"],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }],
   ],
 });
