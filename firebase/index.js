82d3ad2c833b3d9b10cec8b277148b6254164173
MaliaPagley <maliapagley@gmail.com>
2024-01-16 14:29:18 -0500

    Update firebase/Async Storage
    Details:
    -(UPDATE) setup initializing Firebase Auth with Async Storage.

--- firebase/index.js
+++ firebase/index.js
@@ -6,9 +6,10 @@ import {
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_APP_ID,
 } from "@env";
-import { getAuth } from "@firebase/auth";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "@firebase/app";
+import { initializeAuth, getReactNativePersistence } from "@firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
@@ -20,7 +21,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
-const auth = getAuth();
+const db = getFirestore(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
-export { app, db, auth };
+export { auth, app, db };
